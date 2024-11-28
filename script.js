ddb45c73f4ebca4d7c38aa6ff65d0b84a4c82f25
Norman <norman.tetzlaff@gmail.com>
2024-11-28 11:51:33 +0100

    merged, with all branches

--- script.js
+++ script.js
@@ -123,3 +123,26 @@ function gameLoop() {
 }
 gameLoop();
 
+
+// scope block, game timer
+{
+    let timer = 120; 
+    const timerDisplay = document.getElementById('timer');
+
+
+    function runningTimer() {
+        if (timer > 0) {
+            timer--; 
+            timerDisplay.textContent = timer;
+        } else {
+            clearInterval(timerInterval);
+            alert('Timer up');
+        }
+    }
+
+    // show timer ingame
+    const timerInterval = setInterval(() => {
+        runningTimer();
+    }, 1000);
+
+}
